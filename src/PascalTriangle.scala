65fecd2fa698c77efba3f997482733c7d958657b
Vseslav Sekorin <VsSekorin@gmail.com>
2017-01-21 22:45:18 +0300

    Fix main() Pascal Triangle

--- src/PascalTriangle.scala
+++ src/PascalTriangle.scala
@@ -4,7 +4,7 @@ object PascalTriangle {
     else pascal(row - 1, col - 1) + pascal(row - 1, col)
   }
 
-  def main() {
+  def main(args: Array[String]): Unit = {
     println("Pascal's Triangle")
     for (row <- 0 to 10) {
       for (col <- 0 to row) {
